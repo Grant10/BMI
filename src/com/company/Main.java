9c5d6fa08cf47eb96bfcda0cbbcfd6dec83513c3
Grant Caldwell <grant.caldwell.2023@student.lhschools,org>
2019-09-20 09:31:28 -0400

    Added the conversion from pounds to kilograms

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,17 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-    Float height;
+    Float kilograms, meters;
 
 
 
         Scanner Keyboard;
 	Keyboard = new Scanner(System.in);
 	System.out.print("Hi today I am going to claculate your BMI, so what is your heght (inches only)");
-	height = Keyboard.nextfloat;
+	inches = Keyboard.nextfloat;
 	m = inches*0.254
         System.out.print("Next what is your wheight in pounds")
         pounds = keyboard.nextfloat
+        k = pounds*0.453592
         // write your code here
     }
 }
