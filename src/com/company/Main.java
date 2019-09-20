c8d96b085493323f22969fd91c2c2165db2d68a4
Grant Caldwell <grant.caldwell.2023@student.lhschools,org>
2019-09-20 09:24:51 -0400

    Intial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    Float height;
+
+
+
+        Scanner Keyboard;
+	Keyboard = new Scanner(System.in);
+	System.out.print("Hi today I am going to claculate your BMI, so what is your heght (inches only)");
+	height = Keyboard.nextfloat;
+	m = inches*0.254
+        // write your code here
+    }
+}
